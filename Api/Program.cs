92100ca45b62a14415ab8782c2be07eeb4ac627f
Barry Dabrowski <dabrowski.barry@gmail.com>
2024-01-09 13:18:29 +0100

    Api runs test HttpGet work

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,11 +1,24 @@
+using Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+//Add Controller
+builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -13,6 +26,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(Options =>
+{
+    Options.AllowAnyOrigin();
+    Options.AllowAnyHeader();
+    Options.AllowAnyMethod();
+});
 
 app.UseHttpsRedirection();
 
