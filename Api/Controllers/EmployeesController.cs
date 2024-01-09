fbb7c4edc7d25c7a4dde9f007aa96e748b1aaec1
Barry Dabrowski <dabrowski.barry@gmail.com>
2024-01-09 14:59:08 +0100

    Api get's Employees Db and endpoints with GET/POST/DELETE/PUT and cores Origin/Header/Method aktive

--- Api/Controllers/EmployeesController.cs
+++ Api/Controllers/EmployeesController.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Data;
+using Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers
 {
@@ -10,10 +13,82 @@ namespace Api.Controllers
     [Route("api/[controller]")]
     public class EmployeesController : ControllerBase
     {
-        [HttpGet]
-        public string SayHi()
+        private readonly AppDbContext _context;
+
+        public EmployeesController(AppDbContext context)
         {
-            return "HI";
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            var employees = await _context.Employees.AsNoTracking().ToListAsync();
+            return employees;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _context.AddAsync(employee);
+
+            var result = await _context.SaveChangesAsync();
+
+            if (result > 0)
+                return Ok();
+
+            return BadRequest();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee is null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee is null)
+                return NotFound();
+            _context.Remove(employee);
+
+            var result = await _context.SaveChangesAsync();
+            if (result > 0)
+                return Ok("Employee is deleted");
+
+            return BadRequest("Unable to deleted employee");
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> EditEmployee(int id, Employee employee)
+        {
+            var employeeFromDb = await _context.Employees.FindAsync(id);
+
+            if (employeeFromDb is null)
+                return NotFound();
+
+            employeeFromDb.Name = employee.Name;
+            employeeFromDb.Address = employee.Address;
+            employeeFromDb.PhoneNumber = employee.PhoneNumber;
+            employeeFromDb.Email = employee.Email;
+
+            var result = await _context.SaveChangesAsync();
+
+            if (result > 0)
+                return Ok("Employee sucessfully updated");
+
+            return BadRequest("Unable to update data!");
         }
     }
 }
