5c7e9426c07a964da40502fcb33e08b76f652985
Barry Dabrowski <dabrowski.barry@gmail.com>
2024-01-10 12:47:03 +0100

    Client get listing of all employess in UI every employee get a tile  with two buttons: Edit / Delete

--- client/src/employees/employees.component.ts
+++ client/src/employees/employees.component.ts
@@ -2,27 +2,21 @@ import { Component, OnInit, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { EmployeesService } from '../service/employees.service';
 import { Employee } from '../Types/employees';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employees',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, CommonModule],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.css',
 })
 export class EmployeesComponent implements OnInit {
-  employees!: Observable<Employee[]>;
+  employees$!: Observable<Employee[]>;
 
   employeeService = inject(EmployeesService);
 
   ngOnInit(): void {
-    this.employeeService.getEmploees().subscribe({
-      next: (res) => {
-        console.log(res);
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+    this.employees$ = this.employeeService.getEmploees();
   }
 }
